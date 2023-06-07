8093117f21098cadcc75b0f4c358ceb7938ae451
SofiiaBoiarshinova <106270807+SofiiaBoiarshinova@users.noreply.github.com>
2023-06-07 19:33:03 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import pickle
+import cryptocode
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -9,7 +10,19 @@ sock.bind((HOST, PORT))
 sock.listen(1)
 conn, addr = sock.accept()
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+p, g, A = pickle.loads(conn.recv(1024))
+b = 4
+B = g ** b % p
+conn.send(pickle.dumps(B))
+
+K = A ** b % p
+key = str(K)
+msg = 'привет!'
+print('сообщение:', msg)
+
+msgEn = cryptocode.encrypt(msg, key)
+conn.send(pickle.dumps(msgEn))
+print('отправленное сообщение:', msgEn)
 
 conn.close()
+
